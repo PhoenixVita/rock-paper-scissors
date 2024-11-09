1a9dc1622e49f23de8fb5bb2f890fd53446c1be9
PhoenixVita <76767052+PhoenixVita@users.noreply.github.com>
2024-11-09 13:23:10 -0500

    first commit

---
+++ python/rock-paper-scissors.py
@@ -0,0 +1,36 @@
+import random
+
+
+while True:
+    p1 = input('rock, paper, scissors?\n').lower()
+    if (p1 == 'rock'):
+        p1 = 1
+        break
+    elif (p1 == 'paper'):
+        p1 = 2
+        break
+    elif (p1 == 'scissors'):
+        p1 = 3
+        break
+    else:
+        print('\nplease input a valid choice')
+
+
+p2 = random.randrange(0, 4)
+win = 0
+
+
+if (p1 == 1 and p2 == 3 or p1 == 2 and p2 == 1 or p1 == 3 and p2 == 2):
+    win = 1
+elif (p1 == p2):
+    win = 2
+
+
+print('')
+if win == 1:
+    print('Player one wins!')
+elif win == 0:
+    print('Player two wins!')
+else:
+    print('Tie!')
+print('')
